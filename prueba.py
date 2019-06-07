2ebdc45401dd5410b79de83d9d0b70526dbb79ab
alejosanti <Alejoalfredosanti@hotmail.com>
2019-06-07 10:49:29 -0300

    Ahora anda, funcion para buscar una palabra en Wiktionary

--- prueba.py
+++ prueba.py
@@ -10,20 +10,61 @@ articulo --> seccion ---> titulo
 dir(objeto) devuelve una lista de lo que podes pedir
 
 """
-from pattern.web import Wikia
 
-print(dir(Wikia))
+def segunPattern(palabra, tipo):
 
-article = Wikia().search(query = 'cat')
+	import pattern.web as patweb
 
-print(article)
+	#Lista de titulos de secciones
+	tipoSustantivo = ['Sustantivo femenino', 'Sustantivo masculino', 'Sustantivo propio', 'Forma sustantiva']
 
-#article = Wikia().search('cat')
+	tipoAdjetivo = ['Forma adjetiva', 'Adjetivo']
 
-#seccion = article.sections
+	tipoVerbo = ['Forma verbal', 'Verbo', 'Verbo intransitivo', 'Verbo transitivo']
 
-#print(type(seccion))
+	#Se elige la lista correspondiente al tipo seleccionado
+	if (tipo == 'Sustantivo'):
+		tipoElegido = tipoSustantivo
+	elif (tipo == 'Adjetivo'):
+		tipoElegido = tipoAdjetivo
+	elif (tipo == 'Verbo'):
+		tipoElegido = tipoVerbo
 
-#print(seccion)
+	#Se busca el articulo segun la palabra que le pasamos y se guardan los titulos de las secciones de ese articulo
+	engine = patweb.Wiktionary(license=None, throttle=5.0, language='es')
+	query = palabra
+	article = engine.search(query,
+	   start = 1,               # Starting page.
+	   count = 10,              # Results per page.
+	   size = None,
+	   language= 'Español',             # Image size: TINY | SMALL | MEDIUM | LARGE
+	   cached = True)
 
-#print(dir(seccion))
+
+
+	try:
+
+		titulos = []
+		for section in article.sections:
+		     titulos.append(section.title)
+
+		#Se recorre la lista de tipos y se pregunta si alguno coincide con los titulos
+
+		encontrado = False
+
+		print(tipoElegido) #borrar
+		print(titulos) #borrar
+
+		for x in tipoElegido:
+			if(not encontrado == True):
+				encontrado = (x in titulos)
+
+	except AttributeError:
+
+		#En caso de ser una palabra random devuelve None
+		encontrado = None;
+
+	return encontrado
+
+
+print(segunPattern('gradísimo', 'Adjetivo'))
