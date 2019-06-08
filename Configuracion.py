163ac092468ed2b13e586ae1bb4b1aa39b9ca05d
alejosanti <Alejoalfredosanti@hotmail.com>
2019-06-08 11:56:49 -0300

    Configuracion terminada, crea un json

--- Configuracion.py
+++ Configuracion.py
@@ -1,26 +1,52 @@
-# usar elemento Spin para seleccionar la cantidad de sustantivos, adjetivos o verbos que se van a poner en la sopa de letras
+#Funciones que pueden ser útiles para el manejo de ventanas window.Refresh(), windows(keep_on_top = True), window.Disable() window.Disappear(), Reappear() window.Hide(), UnHide()
 
 import PySimpleGUI as sg
 import buscador
+import random
+import json
 
 #Constantes
 
-sustantivos = ['casa', 'auto']
-adjetivos = ['grande', 'rapido']
-verbos = ['juega', 'corre']
-tipografias = ['Helvética', 'Futura', 'Avant Garde', 'Garamond', 'Bodoni', 'Franklin Gothic', 'Myriad', 'Bickham Script', 'Avenir', 'Trajan']
+datos = {}
+datos['orientacion'] = 'Horizontal'
+datos['tipografia'] = 'Arial'
+datos['mayus'] = 'Mayusculas'
+datos['cantSustantivos'] = 0
+datos['cantAdjetivos'] = 0
+datos['cantVerbos'] = 0
+datos['colorSustantivos'] = '#00ff00'
+datos['colorAdjetivos'] = '#0000ff'
+datos['colorVerbos'] = '#ff0000'
+datos['sustantivos'] = []
+datos['adjetivos'] = []
+datos['verbos'] = []
+tipografias = ['Garamond', 'Helvetica', 'Courier', 'Fixedsys', 'Times', 'Verdana']
 
-definicionSustantivos = {}
-definicionAdjetivos = {}
-definicionVerbos = {}
-
-
-
-definiciones = ['dice hola', 'dice como', 'dico estas']
+datos['definicionSustantivos'] = {}
+datos['definicionAdjetivos'] = {}
+datos['definicionVerbos'] = {}
 
 menu_def = [['Abrir', ['Juego', 'Configuración']], ['Seleccionar', ['Tipografia']]]
 
 #funciones
+
+def filtrarSegunCantidad(lista, dic, cant):
+	"""funcion que toma la lista de palabras, elige una cantidad de palabras random
+	y las devuelve en una lista nueva, tambien actualiza el diccionario para que solo
+	queden las claves de la lista"""
+
+	listaRan = []
+	dicRan = {}
+
+	for x in range(int(cant)):
+		elem = random.choice(lista)
+		listaRan.append(elem)
+		lista.remove(elem)
+
+	for palabra in listaRan:
+		dicRan[palabra] = dic[palabra]
+
+	return listaRan, dicRan
 
 def pedirDefinicion():
 	definicion = sg.PopupGetText('Por favor ingrese una definicion de la palabra: ' + palabra, title = 'Ingreso de descripcion')
@@ -39,58 +65,63 @@ pidePalabra = [
 column1 = [
 			[sg.T('Sustantivos', font = ('Arial', 12), key = 'tituloSustantivos')],
 			[sg.Button('Agregar sustantivo', key = 'agregarSustantivo')],
-			[sg.Listbox(values = sustantivos, size=(15, 6), key = 'listboxSustantivos'), sg.T('Cantidad: '), sg.Spin(values = list(map(lambda x: x + 1, (range(len(sustantivos))))), key = 'spinSustantivos', initial_value = '0')],
+			[sg.Listbox(values = datos['sustantivos'], size=(15, 6), key = 'listboxSustantivos'), sg.T('Cantidad: '), sg.Spin(values = list(map(lambda x: x + 1, (range(len(datos['sustantivos']))))), key = 'spinSustantivos', initial_value = '0')],
 			[sg.Button('Eliminar sustantivo', key = 'eliminarSustantivo')],
 			[sg.T('-----------------------')],
 			[sg.ColorChooserButton('Seleccionar color', key = 'colorSustantivos'), sg.B('Cargar color', key = 'cargarColorSustantivos')],
 			[sg.T('-----------------------')],
-			[sg.B('Cargar cantidad')]
+			[sg.B('Cargar cantidad', key = 'cantSustantivos')],
+			[sg.T('Cargado!', visible = False, key = 'cargadoSustantivos')]
 		]
 
 column2 = [
 			[sg.T('Adjetivos', font = ('Arial', 12), key = 'tituloAdjetivos')],
 			[sg.Button('Agregar adjetivos', key = 'agregarAdjetivo')],
-			[sg.Listbox(values = adjetivos, size=(15, 6), key = 'listboxAdjetivos'), sg.T('Cantidad: '), sg.Spin(values = list(map(lambda x: x + 1, (range(len(adjetivos))))), key = 'spinAdjetivos', initial_value = '0')],
+			[sg.Listbox(values = datos['adjetivos'], size=(15, 6), key = 'listboxAdjetivos'), sg.T('Cantidad: '), sg.Spin(values = list(map(lambda x: x + 1, (range(len(datos['adjetivos']))))), key = 'spinAdjetivos', initial_value = '0')],
 			[sg.Button('Eliminar adjetivos', key = 'eliminarAdjetivo')],
 			[sg.T('-----------------------')],
 			[sg.ColorChooserButton('Seleccionar color', key = 'colorAdjetivos'), sg.B('Cargar color', key = 'cargarColorAdjetivos')],
 			[sg.T('-----------------------')],
-			[sg.B('Cargar cantidad')]
+			[sg.B('Cargar cantidad', key = 'cantAdjetivos')],
+			[sg.T('Cargado!', visible = False, key = 'cargadoAdjetivos')]
 		]
 
 column3 = [
 			[sg.T('Verbos', font = ('Arial', 12), key = 'tituloVerbos')],
 			[sg.Button('Agregar verbos', key = 'agregarVerbo')],
-			[sg.Listbox(values = verbos, size=(15, 6), key = 'listboxVerbos'), sg.T('Cantidad: '), sg.Spin(values = list(map(lambda x: x + 1, (range(len(verbos))))), key = 'spinVerbos', initial_value = '0')],
+			[sg.Listbox(values = datos['verbos'], size=(15, 6), key = 'listboxVerbos'), sg.T('Cantidad: '), sg.Spin(values = list(map(lambda x: x + 1, (range(len(datos['verbos']))))), key = 'spinVerbos', initial_value = '0')],
 			[sg.Button('Eliminar verbos', key = 'eliminarVerbo')],
 			[sg.T('-----------------------')],
 			[sg.ColorChooserButton('Seleccionar color', key = 'colorVerbos'), sg.B('Cargar color', key = 'cargarColorVerbos')],
 			[sg.T('-----------------------')],
-			[sg.B('Cargar cantidad')]
+			[sg.B('Cargar cantidad', key = 'cantVerbos')],
+			[sg.T('Cargado!', visible = False, key = 'cargadoVerbos')]
 		]
 
 columnaTipografia = [
-				[sg.InputCombo(values = tipografias, size=(20, 1)), sg.B('Cargar', key = 'cargarTipo')]
+				[sg.InputCombo(values = tipografias, default_value='Helvetica',size=(20, 1), readonly=True, key = 'InputComboTipografias'), sg.B('Cargar', key = 'cargarTipo')]
 			  ]
 
 
 columnaMayus = [
 					[sg.B('Mayúsculas', key = 'mayus')],
 					[sg.B('Minúsculas', key = 'minus')],
-					[sg.T('Texto de muestra', font = ('Arial', 12), key = 'textoPrueba')]
 			   ]
 
 
-columnaHorientacion = [
+columnaOrientacion = [
 						[sg.B('Horizontal', key = 'Bhorizontal')],
 						[sg.B('Vertical', key = 'Bvertical')],
-						[sg.T('Horientación actual: horizontal')]
+						[sg.T('Orientación actual: Horizontal', key = 'orientacionActual')]
 			 		  ]
 
 
 layoutTipografia = [
-		 				[sg.T('Tipografia, horientación y mayus', font = ('Arial', 20))],
-		 				[sg.Column(columnaTipografia, key = 'columnaTipografia'), sg.Column(columnaMayus, key = 'columnaMayus'), sg.Column(columnaHorientacion, key = 'columnaHorientacion')]
+		 				[sg.T('Tipografia, orientación y mayus', font = ('Arial', 20))],
+		 				[sg.Column(columnaTipografia, key = 'columnaTipografia'), sg.Column(columnaMayus, key = 'columnaMayus'),
+						sg.Column(columnaOrientacion, key = 'columnaOrientacion')],
+						[sg.T('TEXTO DE MUESTRA', font = ('Arial', 18), key = 'textoPrueba')],
+						[sg.B('Listo', font = ('Arial', 13))]
 				   ]
 
 layoutPrincipal = [
@@ -128,10 +159,10 @@ while True:
 					definicion = pedirDefinicion()
 
 				#Cargo todos los datos
-				sustantivos.append(palabra)
-				definicionSustantivos[palabra] = definicion
-				window.FindElement('listboxSustantivos').Update(values = sustantivos)
-				window.FindElement('spinSustantivos').Update(values = list(map(lambda x: x + 1, (range(len(sustantivos))))))
+				datos['sustantivos'].append(palabra)
+				datos['definicionSustantivos'][palabra] = definicion
+				window.FindElement('listboxSustantivos').Update(values = datos['sustantivos'])
+				window.FindElement('spinSustantivos').Update(values = list(map(lambda x: x + 1, (range(len(datos['sustantivos']))))))
 
 
 	elif (button == 'agregarAdjetivo'):
@@ -145,10 +176,10 @@ while True:
 				if(reporte == 'El Wikcionario no aprobó la palabra, pero Pattern si.' or reporte == 'Ni el Wikcionario ni Pattern aprobaron la palabra.'):
 					definicion = pedirDefinicion()
 
-				adjetivos.append(palabra)
-				definicionAdjetivos[palabra] = definicion
-				window.FindElement('listboxAdjetivos').Update(values = adjetivos)
-				window.FindElement('spinAdjetivos').Update(values = list(map(lambda x: x + 1, (range(len(adjetivos))))))
+				datos['adjetivos'].append(palabra)
+				datos['definicionAdjetivos'][palabra] = definicion
+				window.FindElement('listboxAdjetivos').Update(values = datos['adjetivos'])
+				window.FindElement('spinAdjetivos').Update(values = list(map(lambda x: x + 1, (range(len(datos['adjetivos']))))))
 
 	elif (button == 'agregarVerbo'):
 		palabra = sg.PopupGetText('Ingrese el verbo:', title = 'Ingreso de palabra', font = ('Arial', 12))
@@ -161,49 +192,49 @@ while True:
 				if(reporte == 'El Wikcionario no aprobó la palabra, pero Pattern si.' or reporte == 'Ni el Wikcionario ni Pattern aprobaron la palabra.'):
 					definicion = pedirDefinicion()
 
-				verbos.append(palabra)
-				definicionVerbos[palabra] = definicion
-				window.FindElement('listboxVerbos').Update(values = verbos)
-				window.FindElement('spinVerbos').Update(values = list(map(lambda x: x + 1, (range(len(verbos))))))
+				datos['verbos'].append(palabra)
+				datos['definicionVerbos'][palabra] = definicion
+				window.FindElement('listboxVerbos').Update(values = datos['verbos'])
+				window.FindElement('spinVerbos').Update(values = list(map(lambda x: x + 1, (range(len(datos['verbos']))))))
 
 	#botones de borrado
 
 	elif (button == 'eliminarSustantivo'):
 		try:
 			palabra = values['listboxSustantivos'][0]
-			sustantivos.remove(palabra)
+			datos['sustantivos'].remove(palabra)
 			#del definicionSustantivos[palabra] ;lo comento por ahora porque todavia no lo estoy cargando
-			window.FindElement('listboxSustantivos').Update(values = sustantivos)
-			if (len(sustantivos) == 0):
+			window.FindElement('listboxSustantivos').Update(values = datos['sustantivos'])
+			if (len(datos['sustantivos']) == 0):
 				window.FindElement('spinSustantivos').Update(values = [0])
 			else:
-				window.FindElement('spinSustantivos').Update(values = list(map(lambda x: x + 1, (range(len(sustantivos))))))
+				window.FindElement('spinSustantivos').Update(values = list(map(lambda x: x + 1, (range(len(datos['sustantivos']))))))
 		except IndexError:
 			sg.Popup('No hay palabra que eliminar')
 
 	elif (button == 'eliminarAdjetivo'):
 		try:
 			palabra = values['listboxAdjetivos'][0]
-			adjetivos.remove(palabra)
+			datos['adjetivos'].remove(palabra)
 			#del definicionAdjetivos[palabra] ;lo comento por ahora porque todavia no lo estoy cargando
-			window.FindElement('listboxAdjetivos').Update(values = adjetivos)
-			if (len(adjetivos) == 0):
+			window.FindElement('listboxAdjetivos').Update(values = datos['adjetivos'])
+			if (len(datos['adjetivos']) == 0):
 				window.FindElement('spinAdjetivos').Update(values = [0])
 			else:
-				window.FindElement('spinAdjetivos').Update(values = list(map(lambda x: x + 1, (range(len(adjetivos))))))
+				window.FindElement('spinAdjetivos').Update(values = list(map(lambda x: x + 1, (range(len(datos['adjetivos']))))))
 		except IndexError:
 			sg.Popup('No hay palabra que eliminar')
 
 	elif (button == 'eliminarVerbo'):
 		try:
 			palabra = values['listboxVerbos'][0]
-			verbos.remove(palabra)
+			datos['verbos'].remove(palabra)
 			#del definicionVerbos[palabra] ;lo comento por ahora porque todavia no lo estoy cargando
-			window.FindElement('listboxVerbos').Update(values = verbos)
-			if (len(verbos) == 0):
+			window.FindElement('listboxVerbos').Update(values = datos['verbos'])
+			if (len(datos['verbos']) == 0):
 				window.FindElement('spinVerbos').Update(values = [0])
 			else:
-				window.FindElement('spinVerbos').Update(values = list(map(lambda x: x + 1, (range(len(verbos))))))
+				window.FindElement('spinVerbos').Update(values = list(map(lambda x: x + 1, (range(len(datos['verbos']))))))
 		except IndexError:
 			sg.Popup('No hay palabra que eliminar')
 
@@ -217,6 +248,7 @@ while True:
 			else:
 				window.FindElement('tituloSustantivos').Update(text_color = colorElegido)
 				window.FindElement('cargarColorSustantivos').Update(button_color= ('white',colorElegido))
+				datos['colorSustantivos'] = colorElegido
 		else:
 			sg.Popup('Tiene que elegir un color antes.')
 
@@ -229,6 +261,7 @@ while True:
 			else:
 				window.FindElement('tituloAdjetivos').Update(text_color = colorElegido)
 				window.FindElement('cargarColorAdjetivos').Update(button_color= ('white',colorElegido))
+				datos['colorAdjetivos'] = colorElegido
 		else:
 			sg.Popup('Tiene que elegir un color antes.')
 
@@ -242,16 +275,64 @@ while True:
 			else:
 				window.FindElement('tituloVerbos').Update(text_color = colorElegido)
 				window.FindElement('cargarColorVerbos').Update(button_color= ('white',colorElegido))
+				datos['colorVerbos'] = colorElegido
 		else:
 			sg.Popup('Tiene que elegir un color antes.')
 
 
 		#Falta que ande el boton "cargar cantidad", para evitar se rompa siempre comprobar que la cantidad de palabras seleccionadas exista
+	elif (button == 'cantSustantivos'):
+		datos['cantSustantivos'] = values['spinSustantivos']
+		window.FindElement('cargadoSustantivos').Update(visible = True)
+
+	elif (button == 'cantAdjetivos'):
+		datos['cantAdjetivos'] = values['spinAdjetivos']
+		window.FindElement('cargadoAdjetivos').Update(visible = True)
+
+	elif (button == 'cantVerbos'):
+		datos['cantVerbos'] = values['spinVerbos']
+		window.FindElement('cargadoVerbos').Update(visible = True)
 
 	elif (button == 'Tipografia'):
+		window.Hide()
 		windowTipografia = sg.Window('Programa').Layout(layoutTipografia)
+		while True:
+			buttonT, valuesT = windowTipografia.Read()
 
+			if(buttonT == 'mayus'):
+				windowTipografia.FindElement('textoPrueba').Update('Texto de muestra'.upper())
+				datos['mayus'] = 'Mayusculas'
 
+			elif(buttonT == 'minus'):
+				windowTipografia.FindElement('textoPrueba').Update('Texto de muestra'.lower())
+				datos['mayus'] = 'Minusculas'
+
+			elif(buttonT == 'Bhorizontal'):
+				windowTipografia.FindElement('orientacionActual').Update('Orientación actual: Horizontal')
+				datos['orientacion'] = 'Horizontal'
+
+			elif(buttonT == 'Bvertical'):
+				windowTipografia.FindElement('orientacionActual').Update('Orientación actual: Vertical')
+				datos['orientacion'] = 'Vertical'
+
+			elif(buttonT == 'cargarTipo'):
+				datos['tipografia'] = valuesT['InputComboTipografias']
+				windowTipografia.FindElement('textoPrueba').Update(font = (datos['tipografia'], 18))
+
+			else:
+				break
+
+		windowTipografia.Close()
+		window.UnHide()
 
 	else:
 		break
+
+datos['sustantivosElegidos'], datos['definicionSustantivos'] = filtrarSegunCantidad(datos['sustantivos'], datos['definicionSustantivos'], datos['cantSustantivos'])
+datos['adjetivosElegidos'], datos['definicionAdjetivos'] = filtrarSegunCantidad(datos['adjetivos'], datos['definicionAdjetivos'], datos['cantAdjetivos'])
+datos['verbosElegidos'], datos['definicionVerbos'] = filtrarSegunCantidad(datos['verbos'], datos['definicionVerbos'], datos['cantVerbos'])
+
+archivo = open('datosConfig.json', 'w')
+json.dump(datos, archivo, indent = 4)
+
+#Falta pasar todos los botones que son iguales a funciones para que queda mas lindo el codigo
